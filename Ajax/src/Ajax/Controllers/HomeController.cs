dfdaf3ab15fc10ca9347f31c8004b64370b6a828
Neil Larion <neil.larion@gmail.com>
2016-05-02 23:07:56 -0700

    Get 4 types of ajax working

---
+++ Ajax/src/Ajax/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Ajax.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ajax.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HelloAjax()
+        {
+            return Content("Hello from the controller!", "text/plain");
+        }
+
+        public IActionResult Sum(int firstNumber, int secondNumber)
+        {
+            return Content((firstNumber + secondNumber).ToString(), "text/plain");
+        }
+
+        public IActionResult DisplayObject()
+        {
+            Destination destination = new Destination("Tokyo", "Japan", 1);
+            return Json(destination);
+        }
+
+        public IActionResult DisplayViewWithAjax()
+        {
+            return View();
+        }
+    }
+}
---
+++ Ajax/src/Ajax/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ajax
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
